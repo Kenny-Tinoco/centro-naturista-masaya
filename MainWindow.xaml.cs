3e9744e4073a7fc92b56bf701835869fddee7713
Kenny_Tinoco <jkennytinoco@hotmail.com>
2021-11-02 20:31:05 -0600

    Se segui desarrollando la interfaz de la ventana principal

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CentroNaturistaMasaya
 {
-    /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -25,11 +13,20 @@ namespace CentroNaturistaMasaya
             InitializeComponent();
 
             /*Sentencias que maximizan la pantalla*/
-            this.Width = SystemParameters.WorkArea.Width;
-            this.Height = SystemParameters.WorkArea.Height;
-            this.Top = SystemParameters.WorkArea.Bottom - this.Height;
-            this.Left = SystemParameters.PrimaryScreenWidth - this.Width;
+            Width = SystemParameters.WorkArea.Width;
+            Height = SystemParameters.WorkArea.Height;
+            Top = SystemParameters.WorkArea.Bottom - Height;
+            Left = SystemParameters.PrimaryScreenWidth - Width;
         }
 
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMinimizar_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
     }
 }
