f53a47dddc58f83243c340316ee6d692c3d03bde
Kenny_Tinoco <jkennytinoco@hotmail.com>
2021-11-18 11:15:47 -0600

    Se desarrolla la ventana de agregar producto

--- UI/Añadir/ucAProducto.xaml.cs
+++ UI/Añadir/ucAProducto.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CentroNaturistaMasaya.UI.Añadir
 {
@@ -21,12 +10,39 @@ namespace CentroNaturistaMasaya.UI.Añadir
         public ucAProducto()
         {
             InitializeComponent();
+            inicializarSpinBox();
         }
 
         private void btnAtrasAgregarProducto_Click(object sender, RoutedEventArgs e)
         {
             if (btnAtrasAgregarProductoClick != null)
                 btnAtrasAgregarProductoClick(sender, e);
+        }
+
+        private void btnAgregarProducto_T_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnIrAExistencia_Click(object sender, RoutedEventArgs e)
+        {
+            if (panelBotonAgregar.Visibility != Visibility.Hidden)
+                panelBotonAgregar.Visibility = Visibility.Hidden;
+
+        }
+
+        private void inicializarSpinBox()
+        {
+            buttonSB.ValueChanged += new RoutedPropertyChangedEventHandler<double>(buttonSB_ValueChanged);
+            cantidadExistencia.Text = "0";
+            buttonSB.Minimum = 0;
+            buttonSB.Maximum = 999;
+            buttonSB.SmallChange = 1;
+        }
+
+        private void buttonSB_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            cantidadExistencia.Text = buttonSB.Value.ToString();
         }
     }
 }
